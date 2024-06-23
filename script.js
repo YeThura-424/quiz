91688a0cbc256b3a185deb2a9410e4134561e997
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-23 21:55:10 +0630

    main function

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ var questions = [
     choices: ["calf", "doe", "goat", "chick"],
   },
 ];
+
+var currentQuestion = 0;
+var correctAnswer = 0;
+var quizOver = false;
+
+$(document).ready(function () {
+  displayCurrentQuestion();
+  $(this).find(".quizMassage").hide();
+  $(this)
+    .find(".nextButton")
+    .on("click", function () {
+      if (!quizOver) {
+        value = $("input[type='radio']:checked").val();
+        if (value == undefined) {
+          $(document).find(".quizMessage").text("Please select an answer");
+          $(document).find(".quizMessage").show();
+        } else {
+          $(document).find(".quizMessage").hide();
+          if (value == questions[currentQuestion].correctAnswer) {
+            correctAnswer++;
+          }
+          currentQuestion++;
+          if (currentQuestion < questions.length) {
+            displayCurrentQuestion();
+          } else {
+            displayScroe();
+            $(document).find(".nextButton").text("Play Again ?");
+            quizOver = true;
+          }
+        }
+      }
+    });
+});
