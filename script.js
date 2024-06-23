073ae3682249ed1f43361eeab77a5467dd6a24a7
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-23 21:44:40 +0630

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var questions = [
+  {
+    question: "What is the baby of a Month know as ?",
+    choices: ["baby", "infant", "kit", "larva"],
+    correctAnswer: 3,
+  },
+  {
+    question: "What is the adult of a kid called",
+    choices: ["calf", "doe", "goat", "chick"],
+  },
+];
